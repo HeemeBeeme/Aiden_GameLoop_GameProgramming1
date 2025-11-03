43c9256afd567dcf801e3941146a3c6a8d850236
Heeme Beeme <heemebeeme@gmail.com>
2025-11-03 19:56:14 -0400

    shark movement, and player death

--- Aiden_GameLoop_GameProgramming1/Program.cs
+++ Aiden_GameLoop_GameProgramming1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Aiden_GameLoop_GameProgramming1
@@ -10,6 +11,17 @@ namespace Aiden_GameLoop_GameProgramming1
     {
         static char Character = 'O';
         static char Shark = '^';
+
+        static int FrameTime = 34;
+
+        static (int, int) SharkPosition = (7, 20);
+        static (int, int) PlayerPosition = (5, 4);
+
+        static float NewSharkPositionX = SharkPosition.Item2;
+        static float NewSharkPositionY = SharkPosition.Item1;
+        static float SharkSpeed = 0.4f;
+
+        static bool isDead = false;
 
         static char[,] Map = { {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
                                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
@@ -26,18 +38,86 @@ namespace Aiden_GameLoop_GameProgramming1
                                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',},
         };
 
-        static void DrawMap()
+        static void Main(string[] args)
         {
-            for(int i = 0; i < Map.GetLength(0); i++)
-            {
-                for(int j = 0; j < Map.GetLength(1); j++)
-                {
-                    if(Map[i, j] == ' ')
-                    {
-                        Console.BackgroundColor = ConsoleColor.Blue;
-                    }
+            Console.CursorVisible = false;
 
-                    Console.Write(Map[i, j]);
+            if(SharkSpeed > 1)
+            {
+                SharkSpeed = 1;
+            }
+            else if(SharkSpeed < 0)
+            {
+                SharkSpeed *= -1;
+            }
+
+                Update();
+
+
+        }
+
+        static void Update()
+        {
+            while (!isDead)
+            {
+                Thread.Sleep(FrameTime);
+
+                ProcessInput();
+                Draw();
+
+                float SharkDistanceX = PlayerPosition.Item2 - SharkPosition.Item2;
+                float SharkDistanceY = PlayerPosition.Item1 - SharkPosition.Item1;
+
+                float Distance = (float)Math.Sqrt(SharkDistanceX * SharkDistanceX + SharkDistanceY * SharkDistanceY);
+                if (Distance > 0)
+                {
+                    NewSharkPositionX += SharkDistanceX / Distance * SharkSpeed;
+                    NewSharkPositionY += SharkDistanceY / Distance * SharkSpeed;
+
+                    float NewSharkX = 0;
+                    float NewSharkY = 0;
+
+                    NewSharkX = (float)Math.Round(NewSharkPositionX);
+                    NewSharkY = (float)Math.Round(NewSharkPositionY);
+
+                    SharkPosition = ((int)NewSharkY, (int)NewSharkX);
+
+                    if (Math.Abs(PlayerPosition.Item2 - SharkPosition.Item2) < 1 && Math.Abs(PlayerPosition.Item1 - SharkPosition.Item1) < 1)
+                    {
+                        isDead = true;
+
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("You Have Died!");
+                    }
+                }
+            }
+        }
+
+        static void Draw()
+        {
+            Console.SetCursorPosition(0, 0);
+
+            for (int i = 0; i < Map.GetLength(0); i++)
+            {
+                for (int j = 0; j < Map.GetLength(1); j++)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+
+                    if (j == PlayerPosition.Item2 && i == PlayerPosition.Item1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write(Character);
+                    }
+                    else if (j == SharkPosition.Item2 && i == SharkPosition.Item1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(Shark);
+                    }
+                    else
+                    {
+                        Console.Write(Map[i, j]);
+                    }
                 }
 
                 Console.WriteLine();
@@ -45,27 +125,13 @@ namespace Aiden_GameLoop_GameProgramming1
             }
             
         }
-        static void Update()
-        {
-
-
-        }
 
         static void ProcessInput()
         {
+            while(Console.KeyAvailable)
+            {
 
-        }
-
-        static void Draw()
-        {
-
-        }
-
-        static void Main(string[] args)
-        {
-            Console.CursorVisible = false;
-            DrawMap();
-            Console.ReadKey();
+            }
         }
     }
 }
